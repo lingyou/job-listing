d18ced2637035b38ab9c21cb11616d6dd1452987
lingyou <fl.socialzone@gmail.com>
2017-01-09 10:58:45 +0800

    add more column to job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,7 @@
 class Job < ApplicationRecord
   validates :title, presence: true
+  validates :wage_uppper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: {greater_than: 0}
+  validates :wage_uppper_bound, numericality: {greater_than_or_equal_to: :wage_lower_bound}
 end
