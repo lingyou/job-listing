e8b67c08ad40056db969d079ec7864422a667f92
lingyou <fl.socialzone@gmail.com>
2017-01-17 19:50:01 +0800

    整理markdown展示的helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 module ApplicationHelper
   def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, no_intra_emphasis: true, fenced_code_blocks: true, disable_indented_code_blocks: true)
+    render_options = {no_links: false, hard_wrap: true, filter_html: true, safe_links_only:true}
+    extensions = {
+          no_intra_emphasis: true,
+          fenced_code_blocks: true
+          }
+    renderer = Redcarpet::Render::HTML.new(render_options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
     return markdown.render(text).html_safe
   end
 end
