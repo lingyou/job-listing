cabee2f679f95f2c24f99b680838296bb8fa4fe5
lingyou <fl.socialzone@gmail.com>
2017-01-30 00:44:31 +0800

    ransack first edition

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'carrierwave'
 gem 'will_paginate'
 gem 'redcarpet'
 gem 'mini_magick'
+gem 'ransack'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :search
   def require_is_admin
     if !current_user.admin?
       redirect_to root_path, alert: "You have no permission."
     end
+  end
+  def search
+    @q = Job.published.ransack(params[:q])
+    @jobs = @q.result(distinct: true)
   end
 end
